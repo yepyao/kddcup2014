76999118302ae17e938b6a160553620630755075
kalenhaha <yepyao@gmail.com>
2014-05-19 16:37:01 +0800

    backup

--- svm_run.sh
+++ svm_run.sh
@@ -7,4 +7,7 @@ echo scale features
 echo begin to train svm
 svm-train -c 1 -g 1 ./model/train.$1.svm_buffer.scale ./T.buf/$1/svm.model
 echo begin to predict by svm model
-svm-predict -b 1 ./model/test.$1.svm_buffer.scale ./T.buf/$1/svm.model ./T.buf/svm_pred.txt
+svm-predict -b 1 ./model/test.$1.svm_buffer.scale ./T.buf/$1/svm.model ./T.buf/$1/svm_pred.txt
+
+echo make submission
+java -cp ./codes/yep/bin evaluation.MakeSubmission ./T.buf/$1/pred.txt test.txt ./T.buf/$1/submission.txt ./data/projectIDMapping
