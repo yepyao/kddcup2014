4a0e8fc504bf91128e6cb2373fe6dca4cd4509bc
kalenhaha <yepyao@gmail.com>
2014-05-19 16:36:01 +0800

    backup

--- svm_run.sh
+++ svm_run.sh
@@ -3,3 +3,8 @@ java -cp codes/yep/bin/ libsvm_interface.MakeInput $1 ./model/
 echo scale features
 ./codes/libsvm/svm-scale -l -1 -u 1 -s ./model/$1.svm_range ./model/train.$1.svm_buffer > ./model/train.$1.svm_buffer.scale
 ./codes/libsvm/svm-scale -r ./model/$1.svm_range ./model/test.$1.svm_buffer > ./model/test.$1.svm_buffer.scale
+
+echo begin to train svm
+svm-train -c 1 -g 1 ./model/train.$1.svm_buffer.scale ./T.buf/$1/svm.model
+echo begin to predict by svm model
+svm-predict -b 1 ./model/test.$1.svm_buffer.scale ./T.buf/$1/svm.model ./T.buf/svm_pred.txt
