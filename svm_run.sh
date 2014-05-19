11a963d4b09c5f2b376e8aa43381e5f24b38f3d7
kalenhaha <yepyao@gmail.com>
2014-05-19 16:39:01 +0800

    backup

--- svm_run.sh
+++ svm_run.sh
@@ -5,9 +5,9 @@ echo scale features
 ./codes/libsvm/svm-scale -r ./model/$1.svm_range ./model/test.$1.svm_buffer > ./model/test.$1.svm_buffer.scale
 
 echo begin to train svm
-svm-train -c 1 -g 1 ./model/train.$1.svm_buffer.scale ./T.buf/$1/svm.model
+./codes/libsvm/svm-train -c 1 -g 1 ./model/train.$1.svm_buffer.scale ./T.buf/$1/svm.model
 echo begin to predict by svm model
-svm-predict -b 1 ./model/test.$1.svm_buffer.scale ./T.buf/$1/svm.model ./T.buf/$1/svm_pred.txt
+./codes/libsvm/svm-predict -b 1 ./model/test.$1.svm_buffer.scale ./T.buf/$1/svm.model ./T.buf/$1/svm_pred.txt
 
 echo make submission
 java -cp ./codes/yep/bin evaluation.MakeSubmission ./T.buf/$1/pred.txt test.txt ./T.buf/$1/submission.txt ./data/projectIDMapping
