c9391fd6e6ef4e811f98f47a9f85f709af9ce420
kalenhaha <yepyao@gmail.com>
2014-05-19 16:47:01 +0800

    backup

--- svm_run.sh
+++ svm_run.sh
@@ -5,7 +5,7 @@ echo scale features
 ./codes/libsvm/svm-scale -r ./model/$1.svm_range ./model/test.$1.svm_buffer > ./model/test.$1.svm_buffer.scale
 
 echo begin to train svm
-./codes/libsvm/svm-train -c 1 -g 1 ./model/train.$1.svm_buffer.scale ./T.buf/$1/svm.model
+./codes/libsvm/svm-train -c 1 -g 1 -e 1 ./model/train.$1.svm_buffer.scale ./T.buf/$1/svm.model
 echo begin to predict by svm model
 ./codes/libsvm/svm-predict -b 1 ./model/test.$1.svm_buffer.scale ./T.buf/$1/svm.model ./T.buf/$1/svm_pred.txt
 
