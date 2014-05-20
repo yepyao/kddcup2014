ee32e165a7fea9d87f9744b852aa6efdf71486de
kalenhaha <yepyao@gmail.com>
2014-05-20 21:05:03 +0800

    backup

--- run_xgboost.sh
+++ run_xgboost.sh
@@ -4,6 +4,9 @@ then
   echo "Usage: <conf name>"
   exit -1
 fi
+rm ./model/train.$1.svm_buffer.buffer
+rm ./model/test.$1.svm_buffer.buffer
+
 echo make feature
 ./T.buf/mkbuf.sh $1 Makefile
 
@@ -18,4 +21,6 @@ xgboost $1.conf task=pred model_in=$round.model
 echo auc evaluation and make submission
 java -cp ../../codes/yep/bin evaluation.MakeSubmission pred.txt ../../test.txt submission.txt ../../data/projectIDMapping
 cd -
+
+
 
