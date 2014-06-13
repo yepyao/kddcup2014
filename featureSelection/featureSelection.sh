3d33629edc9bc35a764fbed24829a9a68c0a1ff1
kalenhaha <yepyao@gmail.com>
2014-06-13 12:05:02 +0800

    backup

--- featureSelection/featureSelection.sh
+++ featureSelection/featureSelection.sh
@@ -1,2 +1,7 @@
-currentLine=$(wc -l < test.txt)
+currentLine=$(wc -l < ../test.txt)
 echo $currentLine
+line=$(head -1 ../model/train.gbrt.svm_buffer)
+read -a array <<<$line
+sum=$((${#array[*]}-1))
+echo $sum
+performance=$(tail -2 ../log.txt)
