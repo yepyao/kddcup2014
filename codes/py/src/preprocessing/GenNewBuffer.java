2e90682c32370dc25af00189ec3f75e717cfde29
kalenhaha <yepyao@gmail.com>
2014-06-13 22:05:05 +0800

    backup

--- codes/py/src/preprocessing/GenNewBuffer.java
+++ codes/py/src/preprocessing/GenNewBuffer.java
@@ -11,29 +11,34 @@ import java.io.OutputStreamWriter;
 public class GenNewBuffer {
 	public static void main(String[] args) throws IOException{
 		double last = Double.valueOf(args[2]);
-		int res = 0;
-		int[] remain = new int[Integer.valueOf(args[1])];
+		double max = last;
+		int maxF = -1;
 		for (int i = 0; i < Integer.valueOf(args[1]); i++){
 			FileInputStream f = new FileInputStream(args[0]+"."+String.valueOf(i));
 			BufferedReader in = new BufferedReader(new InputStreamReader(f));
 			String s = in.readLine();
-			if (Double.valueOf(s) > last){
-				res++;
-				remain[i] = 1;
-			}else remain[i] = 0;
+			if (Double.valueOf(s) > max){
+				max = Double.valueOf(s);
+				maxF = i;
+			}
 			in.close();
 		}
-		System.out.println(res);
 		FileInputStream f = new FileInputStream(args[3]);
 		BufferedReader in = new BufferedReader(new InputStreamReader(f));
 		FileOutputStream f2 = new FileOutputStream(args[4]);
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(f2));
 		String s = in.readLine();
+		String[] temp = s.split(" ");
+		if (maxF != -1) {
+			System.out.println(temp[maxF+1].split(":")[0]);
+			System.out.println(maxF);
+		}
+		else System.out.println(-1);
 		while (s != null){
-			String[] temp = s.split(" ");
+			temp = s.split(" ");
 			out.write(temp[0]);
 			for (int j = 1; j < temp.length; j++)
-				if (remain[j-1] == 0) out.write(" "+temp[j]);
+				if (j != maxF+1) out.write(" "+temp[j]);
 			out.write("\n");
 			s = in.readLine();
 		}
--- featureSelection/featureSelection.sh
+++ featureSelection/featureSelection.sh
@@ -1,4 +1,7 @@
-cd ../xgboost_run/$1/
+cd ..
+./T.buf/mkbuf.sh $1 Makefile &> featureSelection/temp
+java -cp codes/yep/bin/ libsvm_interface.MakeInput $1 ./model/ &> featureSelection/temp
+cd xgboost_run/$1/
 round=0150
 xgboost $1.conf num_round=$round &> ../../featureSelection/temp
 xgboost $1.conf task=pred model_in=$round.model &> ../../featureSelection/temp
@@ -7,27 +10,40 @@ java -cp ../codes/yep/bin evaluation.MakeSubmission pred.txt ../test.txt submiss
 line=$(head -1 result/temp)
 read -a array <<<$line
 best=${array[1]}
-echo "best score:"$best
+echo "initial score:"$best
 cp ../model/train.$1.svm_buffer buffer/train.$1.buffer
 cp ../model/test.$1.svm_buffer buffer/test.$1.buffer
+
 line=$(head -1 ../model/train.$1.svm_buffer)
 read -a array <<<$line
 sum=$((${#array[*]}-1))
-java -cp ../codes/py/bin preprocessing.SplitBuffer buffer/train.$1.buffer $sum
-java -cp ../codes/py/bin preprocessing.SplitBuffer buffer/test.$1.buffer $sum
-for((i=0;i<sum;i++))
-do
-    bash select.sh $1 $i &> temps/temp$i
-done
-wait
 
-java -cp ../codes/py/bin preprocessing.GenNewBuffer result/result.$1 $sum $best buffer/train.$1.buffer buffer/temp.$1.buffer 
-mv buffer/train.$1.buffer buffer/train.$1.buffer.old
-mv buffer/temp.$1.buffer buffer/train.$1.buffer
-java -cp ../codes/py/bin preprocessing.GenNewBuffer buffer/test.$1.buffer $sum $best buffer/test.$1.buffer buffer/temp.$1.buffer > delete
-mv buffer/test.$1.buffer buffer/test.$1.buffer.old
-mv buffer/temp.$1.buffer buffer/test.$1.buffer
-delete=$(head -1 delete)
-echo $delete
-sum=$(($sum-$delete))
-echo $sum
+rd=0
+while :
+do
+    echo "round "$rd":"
+    echo $sum" feature left:"$best
+    java -cp ../codes/py/bin preprocessing.SplitBuffer buffer/train.$1.buffer $sum
+    java -cp ../codes/py/bin preprocessing.SplitBuffer buffer/test.$1.buffer $sum
+    for((i=0;i<sum;i++))
+    do
+        bash select.sh $1 $i &> temps/temp$i
+    done
+    wait
+ 
+    java -cp ../codes/py/bin preprocessing.GenNewBuffer result/result.$1 $sum $best buffer/train.$1.buffer buffer/temp.$1.buffer > delete
+    mv buffer/temp.$1.buffer buffer/train.$1.buffer
+    java -cp ../codes/py/bin preprocessing.GenNewBuffer result/result.$1 $sum $best buffer/test.$1.buffer buffer/temp.$1.buffer 
+    mv buffer/temp.$1.buffer buffer/test.$1.buffer
+    delete=$(head -1 delete)
+    if [ $delelte == -1 ]
+    then
+        break
+    fi
+    echo "feature "$delete" deleted"
+    sum=$(($sum-1))
+    echo $sum
+    index=$(tail -1 delete)
+    best=$(head -1 result/result.$1.$index)
+    rd=$(($rd+1))
+done
