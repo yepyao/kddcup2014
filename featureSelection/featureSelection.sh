99bb61ff2e7fcab72d5f89653e048cc96e98d865
kalenhaha <yepyao@gmail.com>
2014-06-13 13:05:02 +0800

    backup

--- featureSelection/featureSelection.sh
+++ featureSelection/featureSelection.sh
@@ -4,4 +4,6 @@ line=$(head -1 ../model/train.gbrt.svm_buffer)
 read -a array <<<$line
 sum=$((${#array[*]}-1))
 echo $sum
-performance=$(tail -2 ../log.txt)
+tail -2 log.txt >> log2.txt
+line=$(head -1 log2.txt)
+echo $line
